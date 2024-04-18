53b6f573ec25823b3a881a9ec2469674f6e66c9d
Mecasu <melihcansunay@live.com>
2024-04-18 10:34:10 +0300

    reactdoom render fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.js";
 
-ReactDOM.render(
+/*ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
+);*/
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
